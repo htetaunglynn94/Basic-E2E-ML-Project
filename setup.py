9a372f17167dae5320469d81f5b078e80e75eee5
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-06-16 09:56:52 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 from setuptools import find_packages, setup
+from typing import List
 
 
 
+
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+
+    '''
+    This function will return the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
 
 setup(
     name='e2emlproject', 

fa3108c24a9d770a353ac2e51eb8d1b78e79c987
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-07-07 21:41:01 +0800

    Added Hyperparameters

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,7 +41,38 @@ class ModelTrainer:
                       "CatBoosting Classifier": CatBoostRegressor(verbose=False),
                       "AdaBoost Classifier": AdaBoostRegressor()}
 
-            model_report: dict = evaluate_model(X_train, y_train, X_test, y_test, models)
+            params = {"Random Forest": {
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                        },
+                    "Decision Tree": {
+                        "criterion": ["squared_error", "absolute_error", "poisson"]
+                        },
+                    "Gradient Boosting": {
+                        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                        "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                        },
+                    "Linear Regression": {},
+                    "K-Neighbors Classifier": {
+                        "n_neighbors": [3, 5, 7, 9],
+                        "weights": ["uniform", "distance"]
+                        },
+                    "XGBClassifier": {
+                        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                        },
+                    "CatBoosting Classifier": {
+                        "depth": [6, 8, 10],
+                        "learning_rate": [0.01, 0.05, 0.1],
+                        "iterations": [30, 50, 100]
+                        },
+                    "AdaBoost Classifier": {
+                        "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                        }
+                    }
+
+            model_report: dict = evaluate_model(X_train, y_train, X_test, y_test, models, params)
 
             # Get best model score from dictionary
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +18,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            # Implement GridSearchCV to find best parameters
+            gscv = GridSearchCV(estimator = model, 
+                                param_grid = param,
+                                cv = 3)
+            gscv.fit(X_train, y_train)
+            
+            # Train model setting related best hyperparameters
+            model.set_params(**gscv.best_params_)
             model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)

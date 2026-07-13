bc741ab213bf41caafcf182343f604ea7bd3bfee
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-07-13 23:42:31 +0800

    fix syntax error on line 23

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ def predict_datapoint():
             test_preparation_course = request.form.get('test_preparation_course'),
             reading_score = float(request.form.get('reading_score')),
             writing_score = float(request.form.get('writing_score'))
-    
+        )    
 
         pred_df = data.get_data_as_frame()
         predict_pipeline = PredictPipeline()

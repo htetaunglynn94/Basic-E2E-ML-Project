a81086426d4dc98de865e9b9c0e4b68495d273af
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-07-25 13:59:48 +0800

    modify

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=8080)
